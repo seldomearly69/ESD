75b18fa40fed5e86f294abae92effccd7d3bbede
iamAK <akannappan27@gmail.com>
2024-03-03 12:03:00 +0800

    edited user service

--- backend/services/user_service/user.py
+++ backend/services/user_service/user.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 import mysql.connector
 import time
+import bcrypt
 
 app = Flask(__name__)
 # connect to DB
@@ -76,10 +77,10 @@ def register_user():
                 }
             ), 400
 
-
+    encoded_password = data["password_hash"].encode('utf-8')
+    data["password_hash"] = bcrypt.hashpw(encoded_password, bcrypt.gensalt())
     newUser = User(**data)
-
-
+    
     try:
         db.session.add(newUser)
         db.session.commit()
@@ -103,9 +104,48 @@ def register_user():
     ), 201
 
 
-@app.route("/login")
+@app.route("/login", methods=["POST"])
 def login():
-    return "This is login route."
+    print("logging in...")
+    data = request.get_json()
+    print(data)
+    user = db.session.scalars(
+    	db.select(User).filter_by(username=data["username"]).
+    	limit(1)
+    ).first()
+    if (user):
+        encoded_password = data["password_hash"].encode('utf-8')
+        stored_password_hash = user.password_hash.encode('utf-8')
+        password_match = bcrypt.checkpw(encoded_password, stored_password_hash)
+        if(password_match):
+            return jsonify({
+                "code": 200,
+                "data": {
+                    "userId": data["username"],
+                    # supposed to return api-key-auth to front-end??
+                    "api-key-auth": "admin/user"
+                }
+            }), 200
+        else:
+            return jsonify(
+            {
+                "code": 400,
+                "data": {
+                    "userId": data["username"]
+                },
+                "message": "Username or password incorrect!"
+            }
+        ), 400
+    else:
+        return jsonify(
+            {
+                "code": 400,
+                "data": {
+                    "userId": data["username"]
+                },
+                "message": "Username or password incorrect!"
+            }
+        ), 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000, debug=True)
