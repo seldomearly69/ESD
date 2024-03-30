18ce1ba73f30b8946da9f652d938c79fa0e345a7
Cyrus, Tan Jit Young <jityoung.tan@gmail.com>
2024-03-30 18:30:20 +0800

    Finalised simple microservices

--- backend/services/route_service/route_service.js
+++ backend/services/route_service/route_service.js
@@ -71,55 +71,6 @@ app.post('/geocode', async (req, res) => {
 
 
 
-// Route for calculating the route using Mapbox API
-// app.post('/calculate-route', async (req, res) => {
-//     const { locations, currentMode } = req.body;
-
-//     try {
-//         const coordinates = locations.map(location => location.coordinates);
-//         const formattedCoordinates = coordinates.map(coords => coords.join(','));
-
-//         const waypointsOrderResponse = await axios.get(`https://api.mapbox.com/directions-matrix/v1/mapbox/${currentMode}/${formattedCoordinates.join(';')}?access_token=pk.eyJ1IjoiY3lydXMtdGFuIiwiYSI6ImNscTBydXh6ZDAxZGsyaXAxMnV3Y2lwbWEifQ.jfAQQFTfeVegGsuoaNh6Ow`);
-//         const waypointsOrderData = waypointsOrderResponse.data;
-
-//         if (waypointsOrderData.code === 'Ok') {
-//             const waypointsOrder = waypointsOrderData.durations[0].reduce((order, _, index) => {
-//                 order[index] = locations[index];
-//                 return order;
-//             }, []);
-
-//             const orderedCoordinates = waypointsOrder.map(location => location.coordinates);
-
-//             const routeResponse = await axios.get(`https://api.mapbox.com/directions/v5/mapbox/${currentMode}/${orderedCoordinates.join(';')}?geometries=geojson&access_token=pk.eyJ1IjoiY3lydXMtdGFuIiwiYSI6ImNscTBydXh6ZDAxZGsyaXAxMnV3Y2lwbWEifQ.jfAQQFTfeVegGsuoaNh6Ow`);
-//             const routeData = routeResponse.data;
-
-//             if (routeData.routes && routeData.routes.length > 0) {
-//                 const route = routeData.routes[0];
-//                 const routeCoordinates = route.geometry.coordinates;
-
-//                 const distance = route.distance;
-//                 const duration = route.duration;
-
-//                 res.json({ routeCoordinates, distance, duration });
-//             } else {
-//                 res.status(404).json({ error: 'No route found for the selected mode.' });
-//             }
-//         } else {
-//             res.status(404).json({ error: 'An error occurred while calculating waypoints order.' });
-//         }
-//     }
-//     catch (error) {
-//         console.error('Error calculating route:', error);
-//         res.status(500).json({ error: 'An error occurred while calculating the route.' });
-//     }
-// });
-
-
-
-
-
-
-
 // Start the server
 app.listen(PORT, () => {
     console.log(`Route service microservice is listening on port ${PORT}`);
