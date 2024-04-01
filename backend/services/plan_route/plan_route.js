9ed9ea21734779bf314f0e2bede86a3f76504bd8
iamAK <akannappan27@gmail.com>
2024-04-01 21:15:47 +0800

    plan_route swagger

--- backend/services/plan_route/plan_route.js
+++ backend/services/plan_route/plan_route.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const axios = require('axios');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 
-
+const swaggerDocument = YAML.load('./swagger.yaml');
 const app = express();
 const PORT = process.env.PORT || 5013;
 
 app.use(express.json());
 app.use(cors());
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // Endpoint for location validation
 app.post('/validate-location', async (req, res) => {
     const { locationName, userCountry } = req.body;
